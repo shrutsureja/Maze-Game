fb89ae700d48b188a50b905a4909000652d9198c
shrutsureja <shrutsureja.code@gmail.com>
2024-03-13 17:53:25 +0530

    added the vercel web analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
 import MazeGame from './Maze/MazeGame'
 import UnderDevelopment from './components/UnderDevelopment'
+import { inject } from '@vercel/analytics'
 
-
+inject();
 function App() {
 
   return (
